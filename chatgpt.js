6e3750bc279a89a9fd7a55371ab2821acdff4e15
LucasASC23 <139932248+LucasASC23@users.noreply.github.com>
2025-11-17 20:15:14 +0000

    Make a chatbox with chatgpt

--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,27 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+let requestURL="https://api.openai.com/v1/responses"
+let question=prompt("What would you like?")
+async function chatgpt(promptData){
+    const options={
+    
+    method:'Post',
+    headers:{
+        Authorization:'Bearer '+apiKey,
+        "Content-Type":'application/json'
+    },
+    body:JSON.stringify(promptData),
+    model:"gpt-4.1",
+    input:question
+}
+
+const response= await fetch("https://api.openai.com/v1/responses", options)
+const data=await response.json()
+console.log(data.output[0].content[0].text)
+return data
+}
+chatgpt({
+    model:"gpt-4.1",
+    input:question
+})
